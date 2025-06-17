95c6f66f42a246eddb00d6c11b4cad2455634e97
Manish-git-tech <ce240004032@iiti.ac.in>
2025-06-17 23:32:14 +0530

    getting the format of topics in rosbags

---
+++ yolo_detector/bagpy_test.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from rosbags.highlevel import AnyReader
+
+def list_rosbag_topics(bag_path):
+    with AnyReader([Path(bag_path)]) as reader:
+        topics = reader.topics
+        for topic, info in topics.items():
+            print(f"Topic: {topic}")
+            print(f"  Type: {info.msgtype}")
+            print(f"  Message count: {info.msgcount}")
+            print("-" * 40)
+
+# Replace 'your_file.bag' with your actual bag file path
+list_rosbag_topics('..\rosbags\single-object.bag') # change this to your bag file path (discord wale link se download kar lena)
