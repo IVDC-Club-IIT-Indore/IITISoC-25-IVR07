2470ebb72151b4c034470fd41dcee96173e0f3e5
Manish-git-tech <ce240004032@iiti.ac.in>
2025-06-17 23:32:37 +0530

    extracting rgb frames from rosbag

---
+++ yolo_detector/frame_extraction.py
@@ -0,0 +1,21 @@
+from rosbags.highlevel import AnyReader
+from rosbags.image import message_to_cvimage
+import cv2
+import os
+from pathlib import Path
+
+
+bag_path = 'D:\\rosbags\single-object.bag'
+output_dir = 'output/rgb_frames'
+image_topic = '/camera/color/image_raw'  # Adjust as per your bag file
+
+os.makedirs(output_dir, exist_ok=True)
+
+with AnyReader([Path(bag_path)]) as reader:
+    for connection, timestamp, rawdata in reader.messages():
+        if connection.topic == image_topic:
+            msg = reader.deserialize(rawdata, connection.msgtype)
+            img = message_to_cvimage(msg, 'bgr8')  # Convert to OpenCV image
+            filename = os.path.join(output_dir, f'{timestamp}.png')
+            cv2.imwrite(filename, img)
+            print(f"Saved {filename}")
